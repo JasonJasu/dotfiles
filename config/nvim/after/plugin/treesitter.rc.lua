74a0ab043d5a41207465ea1bd35b5c1e1a9e113f
JasonJasu <tejasukmanajason@gmail.com>
2023-12-08 02:49:54 +0700

    adding prettier and null-ls

--- config/nvim/after/plugin/treesitter.rc.lua
+++ config/nvim/after/plugin/treesitter.rc.lua
@@ -2,6 +2,8 @@ local status, ts = pcall(require, 'nvim-treesitter.configs')
 if (not status) then return end
 
 ts.setup {
+  sync_install = false,
+  auto_install = true,
   highlight = {
     enable = true,
     disable = {},
@@ -20,7 +22,13 @@ ts.setup {
     "swift",
     "css",
     "html",
-    "lua"
+    "lua",
+    "javascript",
+    "c",
+    "cpp",
+    "lua",
+    "vim",
+    "rust"
   },
   autotag = {
     enable = true,
