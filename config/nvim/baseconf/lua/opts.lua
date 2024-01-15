a26acb324466a1e8870a9131fc25dcd4b8982dee
JasonJasu <tejasukmanajason@gmail.com>
2024-01-16 03:27:17 +0700

    added baseconf
    added baseconf, removed ll logs and gitignore

---
+++ config/nvim/baseconf/lua/opts.lua
@@ -0,0 +1,54 @@
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.wo.number = true
+
+-- disable conceallevel
+vim.wo.spell = false
+vim.wo.conceallevel = 0
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.shell = "zsh" -- Change opt for default shell, or just comment this
+-- vim.opt.backupskip =
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.ai = true --autoindent
+vim.opt.si = true --smartindent
+vim.opt.wrap = false --no wrap lines
+vim.opt.backspace = "start,eol,indent"
+vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+vim.opt.wildignore:append({ "*/node_modules/*" }) -- for nodemodules
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
+})
+
+-- Add asterisks in block comments
+vim.opt.formatoptions:append({ "r" })
+
+-- Cursor Highlight
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.winblend = 0
+vim.opt.wildoptions = "pum"
+vim.opt.pumblend = 5
+vim.opt.background = "dark"
